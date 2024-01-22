17d89aaffb4eb162454ccd7a990fab7d289a237c
강수정 <kngsujng@naver.com>
2024-01-22 15:38:31 +0900

    Init: Sanity 클라우드 기초 셋팅

---
+++ server/schemas/user.ts
@@ -0,0 +1,54 @@
+export default {
+  title: 'User',
+  name: 'user',
+  type: 'document',
+  fields: [
+    {
+      title: 'Username', // Sanity Studio UI에서 보는 이름
+      name: 'username', // 실제 백엔드/데이터베이스에서 접근할 때 사용하는 이름(key)
+      type: 'string',
+    },
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'string',
+    },
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'user'}]}],
+      validation: (Rule: any) => Rule.unique(),
+    },
+    {
+      title: 'Followers',
+      name: 'followers',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'user'}]}],
+      validation: (Rule: any) => Rule.unique(),
+    },
+    {
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'post'}]}],
+      validation: (Rule: any) => Rule.unique(),
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'username',
+    },
+  },
+}
