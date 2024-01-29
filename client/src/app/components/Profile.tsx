6daf3c27bcf5b6623d76fae86ef6749bbce3f1d6
강수정 <kngsujng@naver.com>
2024-01-29 21:15:34 +0900

    Design: 사용자 프로필, Refator: Profile 컴포넌트

--- client/src/app/components/Profile.tsx
+++ client/src/app/components/Profile.tsx
@@ -1,6 +1,6 @@
 import { Profile } from 'next-auth';
 
-type AvatarSize = 'sm' | 'md' | 'lg';
+type AvatarSize = 'sm' | 'md' | 'lg' | 'xl';
 
 type Props = {
 	image?: string | null;
@@ -15,9 +15,9 @@ export default function Profile({ image, size = 'lg', border = false }: Props) {
 			<img
 				src={image || ''}
 				alt="user image"
-				className={`rounded-full object-cover bg-white ${getImageSizeStyle(
-					size
-				)}`}
+				className={`rounded-full object-cover bg-white ${
+					getImageSizeStyle(size).image
+				}`}
 				referrerPolicy="no-referrer"
 			/>
 		</div>
@@ -29,29 +29,30 @@ const getContainerStyle = (size: AvatarSize, border: boolean): string => {
 	const borderStyle = border
 		? ' bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
 		: '';
-	const sizeStyle = getContainerSize(size);
+	const { container } = getImageSizeStyle(size);
 
-	return `${baseStyle} ${borderStyle} ${sizeStyle}`;
+	return `${baseStyle} ${borderStyle} ${container}`;
 };
 
-function getContainerSize(size: AvatarSize): string {
-	switch (size) {
-		case 'sm':
-			return 'w-9 h-9';
-		case 'md':
-			return 'w-11 h-11';
-		case 'lg':
-			return 'w-[68px] h-[68px]';
-	}
-}
+type ImageSizeStyle = {
+	container: string;
+	image: string;
+};
 
-function getImageSizeStyle(size: AvatarSize): string {
+function getImageSizeStyle(size: AvatarSize): ImageSizeStyle {
 	switch (size) {
 		case 'sm':
-			return 'w-[34px] h-[34px] p-[0.1rem]';
+			return { container: 'w-9 h-9', image: 'w-[34px] h-[34px] p-[0.1rem]' };
 		case 'md':
-			return 'w-[42px] h-[42px] p-[0.1rem]';
+			return { container: 'w-11 h-11', image: 'w-[42px] h-[42px] p-[0.1rem]' };
 		case 'lg':
-			return 'w-16 h-16 p-[0.2rem]';
+			return { container: 'w-[68px] h-[68px]', image: 'w-16 h-16 p-[0.2rem]' };
+		case 'xl':
+			return {
+				container: 'w-[142px] h-[142px]',
+				image: 'w-[138px] h-[138px] p-[0.3rem]',
+			};
+		default:
+			throw new Error(`사이즈 크기가 적절하지 않습니다 : ${size}`);
 	}
 }
