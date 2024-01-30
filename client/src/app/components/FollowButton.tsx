a124319152708e32a0e6008c2377b3cb990e0e10
강수정 <kngsujng@naver.com>
2024-01-30 21:36:11 +0900

    Feat: 팔로우 버튼

--- client/src/app/components/FollowButton.tsx
+++ client/src/app/components/FollowButton.tsx
@@ -10,18 +10,19 @@ type Props = {
 
 export default function FollowButton({ user }: Props) {
 	const { username } = user;
-	const { user: loggedInUser } = useMe();
+	const { user: loggedInUser, toggleFollow } = useMe();
 	const showButton = loggedInUser && loggedInUser.username !== username;
 	const following =
 		loggedInUser &&
-		loggedInUser.following.find((item) => item.username === username);
+		!!loggedInUser.following.find((item) => item.username === username);
 	const text = following ? 'Unfollow' : 'Follow';
+	const handleFollow = () => toggleFollow(user.id, !following);
 	return (
 		<>
 			{showButton && (
 				<Button
 					text={text}
-					onClick={() => {}}
+					onClick={handleFollow}
 					red={text === 'Unfollow'}
 				/>
 			)}
--- client/src/service/user.ts
+++ client/src/service/user.ts
@@ -88,7 +88,7 @@ export async function addBookmark(userId: string, postId: string) {
 	return client
 		.patch(userId) //
 		.setIfMissing({ bookmarks: [] }) //
-		.append('bookmarks', [{ _ref: postId, _type: 'refernce' }]) //
+		.append('bookmarks', [{ _ref: postId, _type: 'reference' }]) //
 		.commit({ autoGenerateArrayKeys: true });
 }
 
@@ -96,5 +96,29 @@ export async function removeBookmark(userId: string, postId: string) {
 	return client
 		.patch(userId) //
 		.unset([`bookmarks[_ref==${postId}]`]) //
+		.commit({ autoGenerateArrayKeys: true });
+}
+
+export async function follow(myId: string, targetId: string) {
+	return client
+		.transaction() //
+		.patch(myId, (user) =>
+			user
+				.setIfMissing({ following: [] })
+				.append('following', [{ _ref: targetId, _type: 'reference' }])
+		)
+		.patch(targetId, (user) =>
+			user
+				.setIfMissing({ followers: [] })
+				.append('followers', [{ _ref: myId, _type: 'reference' }])
+		)
+		.commit({ autoGenerateArrayKeys: true });
+}
+
+export async function unfollow(myId: string, targetId: string) {
+	return client
+		.transaction() //
+		.patch(myId, (user) => user.unset([`following[_ref=="${targetId}"]`]))
+		.patch(targetId, (user) => user.unset([`followers[_ref=="${myId}"]`]))
 		.commit({ autoGenerateArrayKeys: true });
 }
